07cc7e2d6c1ec0a7e0e6f49197012ff8d96b7349
Ciska Vriezenga <ciskavriezenga@gmail.com>
2021-10-13 12:49:54 +0200

    added double forloop example and updated the midpoint displacement example with extra comments

--- csd2a/theorie/6_midpoint_displacement.py
+++ csd2a/theorie/6_midpoint_displacement.py
@@ -5,11 +5,17 @@ import numpy as np
 
 # _________ split function based on midpoint displacement algorithm _________
 def split_note(notes, index, random_range):
+    # splits a note at the passed index, based on midpoint displacement algorith
+    # notes: a list with note dictionaries
+    # index: the index of the note to split
+    # random_range: a floating point defining the range of the randomness
+
     # make sure that the index is not pointing at the last note
     if(index >= len(notes) - 1): return
 
     # calculate new duration for the note and
     # the rest duration value for the new note
+    # split amount --> divide in half, in a quarter, in an eighth
     # for reference of the numpy.random.choice function see:
     # https://numpy.org/doc/stable/reference/random/generated/numpy.random.choice.html
     split_amount = np.random.choice([0.5, 0.25, 0.125], p =[0.45, 0.45, 0.1])
@@ -25,15 +31,11 @@ def split_note(notes, index, random_range):
     # 2. add random value, based on passed randomness
     pitch1 = notes[index]["midi_pitch"]
     pitch2 = notes[index + 1]["midi_pitch"]
-    # round new pitch
-    new_pitch = int((pitch2 - pitch1) / 2.0 + pitch1 + 0.5)
+    # interpolate new pitch and round the result
+    new_pitch = int((pitch2 - pitch1) * split_amount + pitch1 + 0.5)
     # add random value to the new pitch
     new_pitch = new_pitch + random.randint(-1 * random_range, random_range)
-
     notes.insert(index + 1, {"midi_pitch": new_pitch, "qnote_dur": rest_dur})
-
-    print(notes)
-
 
 # _________ prepare for midpoint displacement _________
 note_sequence = [
@@ -49,7 +51,7 @@ random_diminisher = 0.5
 
 # _________ apply midpoint displacement _________
 # outer loop: the number of midpoint displacement iterations of the sequence
-for i in range(3):
+for i in range(3):    
     # calculate 2 ^ i to retrieve the amount of splits in the current iteration
     num_splits = 2 ** i
     # debugging: keep track of splitted note indices
@@ -64,7 +66,7 @@ for i in range(3):
     # lower the random range
     random_range = int(random_range * random_diminisher + 0.5)
     # debugging: print the note indices that are splitted
-    print(splitted_indices)
+    print("splitted_indices:", splitted_indices)
 
 
 # _________ create midi output _________
@@ -82,13 +84,11 @@ mf.addTempo(track, time, 120)
 
 # add the notes from the note_sequence
 for note in note_sequence:
-    print(note)
     dur = note["qnote_dur"]
     mf.addNote(track, channel, note["midi_pitch"],
                 time, dur, volume)
     # increment time based on the duration of the added note
     time = time + dur
-    print(time)
 
 # write midifile to disk
 with open("mysong.mid",'wb') as outf:
