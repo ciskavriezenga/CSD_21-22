18df97ee958af07706f1f0df277352514570c281
Ciska Vriezenga <ciskavriezenga@gmail.com>
2021-10-19 16:31:46 +0200

    minor changes in comments

--- csd2a/theorie/7_modulo.py
+++ csd2a/theorie/7_modulo.py
@@ -1,9 +1,12 @@
 # exmample of the usage of a modulo to get beat index relative to a bar
+
+# for easy debugging, using icecream module
 from icecream import ic
 
 num_beats = 100
 num_beats_per_bar = 7
 
 for beat_index in range(num_beats):
+    # print values
     ic(beat_index)
     ic(beat_index % num_beats_per_bar)
