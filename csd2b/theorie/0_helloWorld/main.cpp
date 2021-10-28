7789c79ab0f77d67db74b9f48780c4ba6901b7bc
Ciska Vriezenga <ciskavriezenga@gmail.com>
2021-10-28 11:20:18 +0200

    added hello world script

---
+++ csd2b/theorie/0_helloWorld/main.cpp
@@ -0,0 +1,11 @@
+// we include iostream to enable console output
+#include <iostream>
+
+int main ()
+{
+  // std is the namespace of the console output functionality
+  std::cout << "Hello world!";
+
+  // end program (line below is not strictly necessary, but a convention)
+  return 0;
+}
