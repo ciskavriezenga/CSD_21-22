80000f3e69a9adbc2baebb7533d12b27dedad9c2
Ciska Vriezenga <ciskavriezenga@gmail.com>
2021-12-01 15:39:18 +0100

    added extra DIY todo for the virtual example - now also make an abstract class :D

--- csd2b/theorie/session_4/02_virtualMethod/main.cpp
+++ csd2b/theorie/session_4/02_virtualMethod/main.cpp
@@ -8,6 +8,7 @@
 
 int main()
 {
+  Oscillator osc;
   /*
    * Create the sine and saw objects dynamically with the keyword 'new'
    * This returns a pointer to the sine object, which we can store in
@@ -36,7 +37,7 @@ int main()
   /*
    * OOF....! - when we call the calculate method of the sine object with an
    * oscillator pointer ... it will go 'terribly wrong'
-   * the calculate function of the baseclas is called, not the subclass method.
+   * the calculate function of the baseclass is called, not the subclass method.
    */
   sineOscillator->calculate();
   sawOscillator->calculate();
@@ -45,12 +46,40 @@ int main()
    * WE NEED THE KEYWORD 'virtual'!
    *
    * _______ * * * DIY * * *  _______
-   * • add the keyword 'virtual' to the header of the baseclass as follow:
+   * 1. add the keyword 'virtual' to the header of the baseclass as follow:
    *  virtual void calculate();
    *
    * now rebuild: make clean, make - because you only altered a header!
    * which calculate method is called?
    *
+   * 2. create an object of the type Oscillator in the main.cpp:
+   * Oscillator osc;
+   * (for the sake of this example there is no need to make this dynamic).
+   * rebuild and rerun your code. Does it work?
+   *
+   * 3. Now, change the virtual method definition ('virtual void calculate()')
+   * in the baseclass into:
+   * virtual void calculate() = 0;
+   * AND remove the implementation of this method in the oscillator.cpp file.
+   * rebuild (rember to run 'make clean' first!) your code --> do you succeed?
+   * ....
+   * ...
+   * ..
+   * .
+   *
+   * NO ...
+   * You will get the following error:
+   * main.cpp:11:14: error: variable type 'Oscillator' is an abstract class
+   * Oscillator osc;
+   *             ^
+   * ./oscillator.h:11:16: note: unimplemented pure virtual method 'calculate' in 'Oscillator'
+   * virtual void calculate() = 0;
+   *
+   * Questions:
+   * Why can't you create an instance of the type Oscillator?
+   * When you view this from the perspective of categories - e.g. Pet, Cat, Dog
+   * does it then 'make sense' that you can not create a Pet? But you can create
+   * a Dog or Cat?
    */
 
 
